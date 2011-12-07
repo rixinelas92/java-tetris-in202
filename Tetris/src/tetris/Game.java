28ca16b9629a6b2dbcacfd6e70359bf4aa5f77f1
gustavo.pacianotto@gmail.com <gustavo.pacianotto@gmail.com@86426023-7125-f6f6-57c1-f636e4403155>
2011-12-07 15:06:39 +0000

    Adding intital files

---
+++ Tetris/src/tetris/Game.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tetris;
+
+/**
+ *
+ * @author felipeteles
+ */
+public class Game {
+    private Screen screen;
+    private Piece currentPiece
+
+}
---
+++ Tetris/src/tetris/Position.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tetris;
+
+/**
+ *
+ * @author felipeteles
+ */
+class Position {
+    private short x;
+    private short y;
+
+    public Position(short newX, short newY){
+
+    }
+    public void setX(short newX){
+    }
+    public void setY(short newY){
+    }
+    public void setPosition(short newX,short newY){
+    }
+    public short getX(){
+        return 0;//mudar
+    }
+    public short getY(){
+        return 0;//mudar
+    }
+
+
+}

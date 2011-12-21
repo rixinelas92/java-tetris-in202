41f8ee1d02fa5bd4f6104b45f7fb48c1ec43402b
gustavo.pacianotto@gmail.com <gustavo.pacianotto@gmail.com@86426023-7125-f6f6-57c1-f636e4403155>
2011-12-21 00:57:27 +0000

    Implementing some methods on Screen and some small fixes on others classes

--- Tetris/src/tetris/Controller.java
+++ Tetris/src/tetris/Controller.java
@@ -13,6 +13,6 @@ public interface Controller {
     public void rotate();
     public void goToBottom();
     public void goLeft();
-    public void goLight();
+    public void goRight();
     public void stop();
 }

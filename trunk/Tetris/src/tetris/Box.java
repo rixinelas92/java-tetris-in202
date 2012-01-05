11acdad67e04a7033f826038e2c25027cec5d504
tacilorib@gmail.com <tacilorib@gmail.com@86426023-7125-f6f6-57c1-f636e4403155>
2012-01-05 22:43:33 +0000

    Elaboration of Javadoc.

--- trunk/Tetris/src/tetris/Box.java
+++ trunk/Tetris/src/tetris/Box.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package tetris;
 
@@ -9,45 +6,76 @@ import java.awt.Color;
 import tetris.Piece.ShapeType;
 
 /**
- *
- * @author felipeteles
+ * This class definies the the basic structure for each box of the screen.
+ * @author felipeteles.
  */
 public class Box {
+
     private boolean full;
     private Color color;
-    
+    /**
+     * Creates a new box with attributes color and full setted.
+     */
     public Box(){
         this.full = false;
         this.color = Piece.shapeColors[ShapeType.None.ordinal()];
     }
+    /** 
+     * Creates a new box.
+     * @param full defines the condition of the box.
+     * @param color defines the color of the box.
+     */
     public Box(boolean full, Color color){
         this.full = full;
         this.color = color;
     }
+    /**
+     * Creates a new box using another one.
+     * @param b identifies the box to be copied.
+     */
     public Box(Box b){
         this.color=b.color;
         this.full = b.full;
     }
+    /**
+     * Default getter of the attribute <em>color</em>.
+     * @return the color of the box.
+     */
     public Color getColor() {
         return color;
     }
-
+    /**
+     * Default setter of the attribute <em>color</em>.
+     * @param color identifies the color of the box to be setted.
+     */
     public void setColor(Color color) {
         this.color = color;
     }
-
+    /**
+     * Reader of the condition of the attribute full.
+     * @return full true if is full and false if empty.
+     */
     public boolean isFull() {
         return full;
     }
-
+    /**
+     * Setter of the attribute full.
+     * @param full identifies the condition of the box.
+     */
     public void setFull(boolean full) {
         this.full = full;
     }
-
-    static Color getEmptyColor(){
+    /**
+     * Default getter of the attributes <em>emptyColor</em>.
+     * @return color for empty boxes.
+     */
+    public static Color getEmptyColor(){
         return Color.WHITE;
     }
-
+    /**
+     * Clones the parameters into the current object.   
+     * @param b identifies the box to be copied.
+     */
     public void copyFrom(Box b){
         this.color = b.color;
         this.full = b.full;
--- trunk/Tetris/src/tetris/Position.java
+++ trunk/Tetris/src/tetris/Position.java
@@ -8,11 +8,14 @@ package tetris;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import tetris.Screen.BorderRetriever;
 import tetris.Screen.FilledRetriever;
 import tetris.Screen.OutOfScreenBoundsException;
 
 /**
- *
+ * This class was designed to manage the position of the boxes in the screen.
+ * It supervises the borders of the screen and allows or not, the mouvement of
+ * a piece.
  * @author felipeteles
  */
 public class Position {
@@ -20,38 +23,89 @@ public class Position {
     private short y;
     static Screen.BorderRetriever borderRetriever;
     static FilledRetriever filledRetriever;
-
+    
+    /**
+    * Setter of the position to the current piece with reference from the last one.
+    * @param old defines the coordanates of the old position.
+    * @throws tetris.Screen.OutOfScreenBoundsException when the borders of the 
+    * screen is not respected.
+    */
     public Position(Position old) throws OutOfScreenBoundsException{
         setX(old.getX());
         setY(old.getY());
         
     }
+    /**
+    * Setter of the coordenates of the parameters <em>x</em> and <em>y</em>.
+    * @param newX defines the next coordenate of the parameter <em>x</em>.
+    * @param newY defines the next coordanate of the parameter <em>y</em>.
+    * @throws tetris.Screen.OutOfScreenBoundsException when the borders of the 
+    * screen is not respected.
+    */
     public Position(int newX,int newY) throws OutOfScreenBoundsException{
         setX((short)newX);
         setY((short)newY);
     }
+    /**
+    * Setter of the coordenates of the parameters <em>x</em> and <em>y</em>.
+    * @param newX defines the next coordenate of the parameter <em>x</em>.
+    * @param newY defines the next coordanate of the parameter <em>y</em>.
+    * @throws tetris.Screen.OutOfScreenBoundsException when the borders of the 
+    * screen is not respected.
+    */
     public Position(short newX, short newY) throws OutOfScreenBoundsException{
         setX(newX);
         setY(newY);
     }
+    /* Hidding constructor from user.
+     */
     private Position(){
     }
+    /**
+    * Default setter of the coordanate <em>x</em>.
+    * @param newX defines the next coordanate of the parameter <em>x</em>.
+    * @throws tetris.Screen.OutOfScreenBoundsException when the border of the 
+    * screen is not respected.
+    */
     public void setX(int newX) throws OutOfScreenBoundsException{
         setX((short)newX);
     }
+    /**
+    * Default setter of the coordanate <em>y</em>.
+    * @param newY defines the next coordanate of the parameter <em>y</em>.
+    * @throws tetris.Screen.OutOfScreenBoundsException when the border of the 
+    * screen is not respected.
+    */
     public void setY(int newY) throws OutOfScreenBoundsException {
         setY((short)newY);
     }
+    /**
+    * Default setter of the coordanate <em>x</em>.
+    * @param newX defines the next coordanate of the parameter <em>x</em>.
+    * @throws tetris.Screen.OutOfScreenBoundsException when the border of the 
+    * screen is not respected.
+    */
     public void setX(short newX)throws OutOfScreenBoundsException{
         if(newX-1 > borderRetriever.getMaxX() || newX+1 < 0)
             throw new OutOfScreenBoundsException();
         x = newX;
     }
+    /**
+    * Default setter of the coordanate <em>y</em>.
+    * @param newY defines the next coordanate of the parameter <em>y</em>.
+    * @throws tetris.Screen.OutOfScreenBoundsException when the border of the 
+    * screen is not respected.
+    */
     public void setY(short newY)throws OutOfScreenBoundsException{
         if(newY-1 > borderRetriever.getMaxY() || newY+1 < 0)
             throw new OutOfScreenBoundsException();
         y = newY;
     }
+    /**
+    * Default setter of the coordenates <em>x</em> and <em>y</em>. 
+    * @param newX defines the next coordenate of the parameter <em>x</em>.
+    * @param newY defines the next coordanates of the parameter <em>y</em>. 
+    */
     public void setPosition(short newX,short newY){
        try{
             setX(newX);
@@ -60,19 +114,42 @@ public class Position {
             e.printStackTrace();
         }
     }
+    /**
+    * Default getter of the attribute <em>x</em>
+    * @return the coordenate x of the box.
+    */
     public short getX(){
-        return x;//mudar
+        return x;
     }
+    /**
+    * Default getter of the attribute <em>y</em>
+    * @return the coordenate y of the box.
+    */
     public short getY(){
-        return y;//mudar
+        return y;
     }
-    static public void setBorderRetriever(Screen.BorderRetriever borderRetriever){
+    /**
+    * Default setter of the parameter <em>borderRetriever</em> definig the new 
+    * borders.
+    * @param borderRetriever defines the next dimension.
+    */
+    public static void setBorderRetriever(BorderRetriever borderRetriever){
         Position.borderRetriever = borderRetriever;
     }
-    static public void setFilledRetriever(Screen.FilledRetriever filledRetriever){
+    /**
+    * Default setter of the parameter <em>filledRetriever</em> definig the new 
+    * surface with statics box.
+    * @param filledRetriever informes if the borders or statics boxes were reached.
+    */
+    public static void setFilledRetriever(Screen.FilledRetriever filledRetriever){
         Position.filledRetriever = filledRetriever;
     }
-
+    /**
+    * Returns true if the coordenates x and y is filled with a static box.
+    * @param x defines the coordanate x of the box.
+    * @param y defines the coordenate y of the box.
+    * @return true if a box is already positioned in the coordenates (x,y).
+    */
     static boolean isFilled(int x, int y){
         return filledRetriever.isFilledWithStaticBloc(x, y);
     }

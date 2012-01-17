5bb81497c8a7163d53406a345e60a7f742617ac6
gustavo.pacianotto@gmail.com <gustavo.pacianotto@gmail.com@86426023-7125-f6f6-57c1-f636e4403155>
2012-01-17 09:26:32 +0000

    Tetris Preferences saving to file

--- trunk/Tetris/src/tetris/util/TetrisPreferences.java
+++ trunk/Tetris/src/tetris/util/TetrisPreferences.java
@@ -6,8 +6,9 @@
 package tetris.util;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,7 +19,16 @@ import java.util.logging.Logger;
  */
 
 public class TetrisPreferences {
-    Properties p;
+    private Properties p;
+
+    enum ImplementedProperties{
+        INT_KEYPAUSE, INT_KEYGOLEFT, INT_KEYGORIGHT, INT_KEYGODOWN, INT_KEYROTATE, INT_KEYDOWN, INT_KEYHOLD,
+        STR_USERNAME, STR_IP
+
+    }
+
+    
+
 
     public void readProperties(){
         FileInputStream propFile = null;
@@ -26,6 +36,7 @@ public class TetrisPreferences {
             propFile = new FileInputStream("properties.tet");
             p = new Properties(System.getProperties());
             p.load(propFile);
+
         } catch (IOException ex) {
             Logger.getLogger(TetrisPreferences.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
@@ -36,6 +47,56 @@ public class TetrisPreferences {
             }
         }
     }
+
+    public void saveProperties(){
+        FileOutputStream propFile = null;
+        PrintStream ps = null;
+        try {
+            propFile = new FileOutputStream("properties.tet");
+            ps = new PrintStream(propFile);
+            p.list(ps);
+        } catch (IOException ex) {
+            Logger.getLogger(TetrisPreferences.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try{
+                ps.close();
+            }catch (Exception e){}
+            
+            try {
+                propFile.close();
+            } catch (IOException ex) {
+                Logger.getLogger(TetrisPreferences.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    void setProperty(ImplementedProperties key, Object o){
+        if(key.toString().startsWith("INT")){
+            if(Integer.class.isInstance(o)){
+                p.put(key, o);
+            }
+        } else if(key.toString().startsWith("STR")){
+            if(String.class.isInstance(o)){
+                p.put(key, o);
+            }
+        }
+    }
+    
+    public Integer getIntProperty(ImplementedProperties key){
+        Object o = p.getProperty(key.toString());
+        if(Integer.class.isInstance(o)){
+            return (Integer)o;
+        }
+        return null;
+    }
+
+    public String getStrProperty(ImplementedProperties key){
+        Object o = p.getProperty(key.toString());
+        if(String.class.isInstance(o)){
+            return (String)o;
+        }
+        return null;
+    }
+
 
 
 }

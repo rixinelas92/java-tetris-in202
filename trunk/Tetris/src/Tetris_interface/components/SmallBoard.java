3d6a576d510799b59b0aca79009923a5a129f406
gustavo.pacianotto@gmail.com <gustavo.pacianotto@gmail.com@86426023-7125-f6f6-57c1-f636e4403155>
2012-02-09 23:27:20 +0000

    Improvemments on SmallBoard

--- trunk/Tetris/src/Tetris_interface/components/SmallBoard.java
+++ trunk/Tetris/src/Tetris_interface/components/SmallBoard.java
@@ -6,7 +6,6 @@
 package Tetris_interface.components;
 
 import Tetris_interface.Interface;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import javax.swing.ImageIcon;
@@ -30,18 +29,22 @@ public class SmallBoard extends JPanel {
         setSize(Screen.SIZE_X * pxlsize, Screen.SIZE_Y * pxlsize);
         boardLabels = new JLabel[Screen.SIZE_X][Screen.SIZE_Y];
         setBackground(Interface.COLOR_semiopaque);
+        setDoubleBuffered(true); 
         GridLayout layout = new GridLayout(Screen.SIZE_Y,Screen.SIZE_X,0,0);
         setLayout(layout);
         isFilled = new int[Screen.SIZE_X];
+        ImageIcon img = new ImageIcon(container.getClass().getResource("imgs/small6.png"));
         for (int i = 0; i < Screen.SIZE_X; i++) {
             isFilled[i] = 0;
             for (int j = 0; j < Screen.SIZE_Y; j++) {
-                boardLabels[i][j] = new JLabel(new ImageIcon(getClass().getResource("../imgs/small6.png")));
+                
+                boardLabels[i][j] = new JLabel(img);
                 boardLabels[i][j].setSize(pxlsize, pxlsize);
                 boardLabels[i][j].setMaximumSize(new Dimension(pxlsize, pxlsize));
                 boardLabels[i][j].setMinimumSize(new Dimension(pxlsize, pxlsize));
-
+                boardLabels[i][j].setOpaque(true);
                 boardLabels[i][j].setVisible(false);
+                
                 add(boardLabels[i][j]);
 
             }
@@ -58,23 +61,30 @@ public class SmallBoard extends JPanel {
 
 
     public void updateBoardDescription(int newFilled[]) {
+        boolean changed = false;
         for (int i = 0; i < Screen.SIZE_X; i++) {
             int diff = this.isFilled[i] ^ newFilled[i];
             if (diff == 0) {
                 continue;
             }
+            changed = true;
             for (int j = 0; j < Screen.SIZE_Y; j++) {
                 if ((diff & (1 << j)) > 0) {
                     if ((newFilled[i] & (1 << j)) > 0) {
                         boardLabels[i][j].setVisible(true);
                     } else {
                         boardLabels[i][j].setVisible(false);
+                        paintImmediately(boardLabels[i][j].getBounds());
                     }
                 }
             }
             isFilled[i] = newFilled[i];
 
         }
+        if(changed){
 
+            paintImmediately(getBounds());
+            
+        }
     }
 }
